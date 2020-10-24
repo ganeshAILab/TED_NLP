57495d85c2e655f36f6c990cb44b7e2081d21d94
ganeshAIlab <ganeshAIlab@gmail.com>
2020-10-24 10:35:39 +0530

    Added Ipynb and the data file

--- utils.py
+++ utils.py
@@ -13,46 +13,46 @@ from textblob import TextBlob
 nlp = spacy.load('en_core_web_sm')
 
 
-def _get_wordcounts(x):
+def get_wordcounts(x):
     length = len(str(x).split())
     return length
 
 
-def _get_charcounts(x):
+def get_charcounts(x):
     s = x.split()
     x = ''.join(s)
     return len(x)
 
 
-def _get_avg_wordlength(x):
-    count = _get_charcounts(x) / _get_wordcounts(x)
+def get_avg_wordlength(x):
+    count = get_charcounts(x) / get_wordcounts(x)
     return count
 
 
-def _get_stopwords_counts(x):
+def get_stopwords_counts(x):
     l = len([t for t in x.split() if t in stopwords])
     return l
 
 
-def _get_hashtag_counts(x):
+def get_hashtag_counts(x):
     l = len([t for t in x.split() if t.startswith('#')])
     return l
 
 
-def _get_mentions_counts(x):
+def get_mentions_counts(x):
     l = len([t for t in x.split() if t.startswith('@')])
     return l
 
 
-def _get_digit_counts(x):
+def get_digit_counts(x):
     return len([t for t in x.split() if t.isdigit()])
 
 
-def _get_uppercase_counts(x):
+def get_uppercase_counts(x):
     return len([t for t in x.split() if t.isupper()])
 
 
-def _cont_exp(x):
+def cont_exp(x):
     contractions = {
         "ain't": "am not",
         "aren't": "are not",
@@ -148,52 +148,52 @@ def _cont_exp(x):
         return x
 
 
-def _get_emails(x):
+def get_emails(x):
     emails = re.findall(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+\b)', x)
     counts = len(emails)
 
     return counts, emails
 
 
-def _remove_emails(x):
+def remove_emails(x):
     return re.sub(r'([a-z0-9+._-]+@[a-z0-9+._-]+\.[a-z0-9+_-]+)', "", x)
 
 
-def _get_urls(x):
+def get_urls(x):
     urls = re.findall(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', x)
     counts = len(urls)
 
     return counts, urls
 
 
-def _remove_urls(x):
+def remove_urls(x):
     return re.sub(r'(http|https|ftp|ssh)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?', '', x)
 
 
-def _remove_rt(x):
+def remove_rt(x):
     return re.sub(r'\brt\b', '', x).strip()
 
 
-def _remove_special_chars(x):
+def remove_special_chars(x):
     x = re.sub(r'[^\w ]+', "", x)
     x = ' '.join(x.split())
     return x
 
 
-def _remove_html_tags(x):
+def remove_html_tags(x):
     return BeautifulSoup(x, 'lxml').get_text().strip()
 
 
-def _remove_accented_chars(x):
+def remove_accented_chars(x):
     x = unicodedata.normalize('NFKD', x).encode('ascii', 'ignore').decode('utf-8', 'ignore')
     return x
 
 
-def _remove_stopwords(x):
+def remove_stopwords(x):
     return ' '.join([t for t in x.split() if t not in stopwords])
 
 
-def _make_base(x):
+def make_base(x):
     x = str(x)
     x_list = []
     doc = nlp(x)
@@ -207,25 +207,25 @@ def _make_base(x):
     return ' '.join(x_list)
 
 
-def _get_value_counts(df, col):
+def get_value_counts(df, col):
     text = ' '.join(df[col])
     text = text.split()
     freq = pd.Series(text).value_counts()
     return freq
 
 
-def _remove_common_words(x, freq, n=20):
+def remove_common_words(x, freq, n=20):
     fn = freq[:n]
     x = ' '.join([t for t in x.split() if t not in fn])
     return x
 
 
-def _remove_rarewords(x, freq, n=20):
+def remove_rarewords(x, freq, n=20):
     fn = freq.tail(n)
     x = ' '.join([t for t in x.split() if t not in fn])
     return x
 
 
-def _spelling_correction(x):
+def spelling_correction(x):
     x = TextBlob(x).correct()
     return x
